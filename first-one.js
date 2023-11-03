25d5987ce12725e50077622d2e633447a8fc04e1
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-11-03 15:05:10 +0530

    arrow-obj

--- first-one.js
+++ first-one.js
@@ -1,2 +1,15 @@
-const fs =require('fs');
-fs.writeFileSync('hello.text','hello world');
+// const fs =require('fs');
+// fs.writeFileSync('hello.text','hello world');
+
+let sum=(a,b)=>{
+    return a*b
+}
+console.log(sum(3,4))
+ 
+
+const student={
+    name : "Gaurav",
+    age : 26,
+    subject : "nodejs",
+    year:23
+}
