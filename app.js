f43f1c23731dadd006fb7a45324385e00efe1193
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-11-20 16:16:32 +0530

    middleware1

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const http=require('http');
+const express= require('express');
 
-const routes=require('./routes');
+const app=express()
 
-console.log(routes.someText)
-const server=http.createServer(routes.handler);
+app.use((req,res,next)=>{
+    console.log('in the middleWare')
+    next()
+});
+app.use((req,res,next)=>{
+    console.log('in other middleware')
+    res.send('<h1>Hello from Express!</h1>')
+})
 
-server.listen(3000)
+
+// console.log(routes.someText)
+// const server=http.createServer();
+
+// server.listen(3000)
+app.listen(3000)
