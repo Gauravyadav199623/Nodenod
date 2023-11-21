119b622193b8c7d78f130e42d442830e43f0f197
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-11-21 16:21:17 +0530

    Filter and routes

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express= require('express');
 const bodyParser= require('body-parser')
 
-const app=express()
+const app=express();
+
+const adminRoutes= require('./routes/admin');
+const shopRoutes= require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/add-product',(req,res,next)=>{
-    console.log('in other middleware')
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
-    // next()// allow request to continue to next middleware in line
-});
-app.post('/product',(req,res,next)=>{
-    console.log(req.body)
-    res.redirect('/')
+app.use('/admin',adminRoutes)
+app.use(shopRoutes)
+
+
+app.use((req,res,next)=>{
+    res.status(404).send("<h1>Page not found</h1>")
 })
-app.use('/',(req,res,next)=>{
-    console.log('in other middleware1')
-    res.send('<h1>Hello from Express!</h1>')
-});
+
 
 // const server=http.createServer();
 
