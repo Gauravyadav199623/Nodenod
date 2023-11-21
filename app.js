3c5434735232014adca5c3593027b238c4e6ddd7
gauravyadav199623 <gauravyadav199623@gmail.com>
2023-11-21 12:13:19 +0530

    parser,next

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const express= require('express');
+const bodyParser= require('body-parser')
 
 const app=express()
 
-app.use((req,res,next)=>{
-    console.log('in the middleWare')
-    next()
-});
-app.use((req,res,next)=>{
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
     console.log('in other middleware')
-    res.send('<h1>Hello from Express!</h1>')
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
+    // next()// allow request to continue to next middleware in line
+});
+app.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
 })
+app.use('/',(req,res,next)=>{
+    console.log('in other middleware1')
+    res.send('<h1>Hello from Express!</h1>')
+});
 
-
-// console.log(routes.someText)
 // const server=http.createServer();
 
 // server.listen(3000)
